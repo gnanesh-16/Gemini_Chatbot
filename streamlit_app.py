c36642b98c06d2e436351d40faaf2a9ca4b086ff
Gnanesh Balusa <98212179+gnanesh-16@users.noreply.github.com>
2024-01-16 13:40:11 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,46 +1,39 @@
-# import os
-# import textwrap
-# import streamlit as st
-# import google.generativeai as genai
-
-
-# genai.configure(api_key=os.getenv("GOOGLE_GEMINI_KEY"))
-# model = genai.GenerativeModel('gemini-pro')
-
-# if "chat" not in st.session_state:
-#     st.session_state.chat = model.start_chat(history=[])
-
-# if (prompt := st.text_input("What can I do for you?")):
-    
-#     response = st.session_state.chat.send_message(prompt)
-
-#     response_markdown = textwrap.dedent(response.text).strip()
-#     with st.chat_message("assistant"):
-#         st.markdown(response_markdown, unsafe_allow_html=True)
-
-# if "chat" in st.session_state:
-#     for message in st.session_state.chat.history:
-#         with st.chat_message("user" if message.role == "user" else "assistant"):
-#             st.markdown(textwrap.dedent(message.parts[0].text).strip(), unsafe_allow_html=True)
-            
-import os
-import textwrap
 import streamlit as st
+import os
 import google.generativeai as genai
 
+# Initialize Gemini-Pro 
 genai.configure(api_key=os.getenv("GOOGLE_GEMINI_KEY"))
 model = genai.GenerativeModel('gemini-pro')
 
+# Gemini uses 'model' for assistant; Streamlit uses 'assistant'
+def role_to_streamlit(role):
+  if role == "model":
+    return "assistant"
+  else:
+    return role
+
+# Add a Gemini Chat history object to Streamlit session state
 if "chat" not in st.session_state:
-    st.session_state.chat = model.start_chat(history=[])
+    st.session_state.chat = model.start_chat(history = [])
 
-if "chat" in st.session_state:
-    for message in reversed(st.session_state.chat.history):
-        with st.chat_message("user" if message.role == "user" else "assistant"):
-            st.markdown(textwrap.dedent(message.parts[0].text).strip(), unsafe_allow_html=True)
+# Display Form Title
+st.title("Chat with Google Gemini-Pro!")
 
-if (prompt := st.text_input("What can I do for you?")):
-    response = st.session_state.chat.send_message(prompt)
-    response_markdown = textwrap.dedent(response.text).strip()
+# Display chat messages from history above current input box
+for message in st.session_state.chat.history:
+    with st.chat_message(role_to_streamlit(message.role)):
+        st.markdown(message.parts[0].text)
+
+# Accept user's next message, add to context, resubmit context to Gemini
+if (prompt := st.text_input("I possess a well of knowledge. What would you like to know?")):
+    # Display user's last message
+    with st.chat_message("user"):
+        st.markdown(prompt)
+    
+    # Send user entry to Gemini and read the response
+    response = st.session_state.chat.send_message(prompt) 
+    
+    # Display last 
     with st.chat_message("assistant"):
-        st.markdown(response_markdown, unsafe_allow_html=True)
+        st.markdown(response.text)
