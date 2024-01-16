886d2be0d61f7fb5c4cafd3e4aeb3f9e24be1e29
Gnanesh Balusa <98212179+gnanesh-16@users.noreply.github.com>
2024-01-16 15:06:31 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,18 +7,25 @@
 # model = genai.GenerativeModel('gemini-pro')
 
 # def role_to_streamlit(role):
-#   if role == "model":
-#     return "assistant"
-#   else:
-#     return role
+#     if role == "model":
+#         return "assistant"
+#     else:
+#         return role
+
+# # Function to convert chat history to text
+# def convert_chat_to_text(chat_history):
+#     text = ""
+#     for message in chat_history:
+#         text += f"{role_to_streamlit(message.role)}: {message.parts[0].text}\n"
+#     return text
 
 # # Add a Gemini Chat history object to Streamlit session state
 # if "chat" not in st.session_state:
-#     st.session_state.chat = model.start_chat(history = [])
+#     st.session_state.chat = model.start_chat(history=[])
 
 # st.title("Chat with Google Gemini-Pro!")
 
-# # Display chat messages from history above current input box
+# # Display chat messages from history above the current input box
 # for message in st.session_state.chat.history:
 #     with st.chat_message(role_to_streamlit(message.role)):
 #         st.markdown(message.parts[0].text)
@@ -27,11 +34,21 @@
 # if prompt := st.chat_input("I possess a well of knowledge. What would you like to know?"):
 #     # Display user's last message
 #     st.chat_message("user").markdown(prompt)
-    
+
 #     # Send user entry to Gemini and read the response
-#     response = st.session_state.chat.send_message(prompt) 
+#     response = st.session_state.chat.send_message(prompt)
 #     with st.chat_message("assistant"):
 #         st.markdown(response.text)
+
+# # Add a button to download the chat history as a text file
+# if st.button("Download Chat History"):
+#     chat_text = convert_chat_to_text(st.session_state.chat.history)
+#     st.download_button(
+#         label="Download Chat",
+#         data=chat_text,
+#         file_name="chat_history.txt",
+#         key="download_button"
+#     )
 
 import streamlit as st
 import os
@@ -57,6 +74,10 @@ def convert_chat_to_text(chat_history):
 # Add a Gemini Chat history object to Streamlit session state
 if "chat" not in st.session_state:
     st.session_state.chat = model.start_chat(history=[])
+
+# Logo
+google_logo_url = "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"
+st.image(google_logo_url, use_column_width=True)
 
 st.title("Chat with Google Gemini-Pro!")
 
@@ -84,3 +105,4 @@ if st.button("Download Chat History"):
         file_name="chat_history.txt",
         key="download_button"
     )
+
